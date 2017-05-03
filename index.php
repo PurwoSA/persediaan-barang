ed8f16cbed2bd9d83a13a2966f0ca91d47b27436
Purwo Setyo Aji <purwo.setyo.aji@gmail.com>
2017-05-03 12:59:03 +0700

    bisa

--- index.php
+++ index.php
@@ -174,10 +174,8 @@
       // This function is called from the pop-up menus to transfer to
       // a different page. Ignore if the value returned is a null string:
       function goPage(newURL) {
-
         // if url is empty, skip the menu dividers and reset the menu selection to default
         if (newURL != "") {
-
           // if url is "-", it is this page -- reset the menu:
           if (newURL == "-") {
             resetMenu();
@@ -188,12 +186,10 @@
           }
         }
       }
-
       // resets the menu selection upon entry to this page:
       function resetMenu() {
         document.gomenu.selector.selectedIndex = 2;
       }
     </script>
   </body>
-
   </html>
