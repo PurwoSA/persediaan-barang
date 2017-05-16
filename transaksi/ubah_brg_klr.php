1c5d04d8bb54072078758d5e5da22583efbbc93a
PurwoSA <purwo.setyo.aji@gmail.com>
2017-05-16 16:13:28 +0700

    CRUD Barang Keluar

--- transaksi/ubah_brg_klr.php
+++ transaksi/ubah_brg_klr.php
@@ -1,25 +1,28 @@
 <?php
 include 'header.php';
 
+if(!isset($_GET['id'])){
+    die("Error: ID Tidak Dimasukkan");
+}
+
 //Ambil data
-$query  = $db->prepare("SELECT MAX(no_klr) AS palingGede FROM barang_klr");
+$query  = $db->prepare("SELECT * FROM staf x, barang y, barang_klr z WHERE z.no_klr = :no_klr AND x.nip = z.nip AND y.kd_brg = z.kd_brg");
 $query1 = $db->prepare("SELECT nip, nm_staf FROM staf");
 $query2 = $db->prepare("SELECT kd_brg, nm_brg FROM barang");
+$query->bindParam(":no_klr", $_GET['id']);
 //Jalankan perintah SQL
 $query->execute();
+$query1->execute();
+$query2->execute();
 if ($query->rowCount() == 0) {
     //Tidak ada hasil
-    $kode = 1;
+    die("Error: ID Tidak Ditemukan");
 } else {
     // ID Ditemukan, Ambil data
     $data = $query->fetch();
-    $kode = $data['palingGede'] + 1;
+    $data1 = $query1->fetchAll();
+    $data2 = $query2->fetchAll();
 }
-$query1->execute();
-$query2->execute();
-// Ambil semua data dan masukkan ke variable $data
-$data1 = $query1->fetchAll();
-$data2 = $query2->fetchAll();
 
 if(isset($_POST['submit'])){
     // Simpan data yang di inputkan ke POST ke masing-masing variable dan convert semua tag HTML yang mungkin dimasukkan untuk mengindari XSS
@@ -29,13 +32,13 @@ if(isset($_POST['submit'])){
     $tgl_klr = htmlentities($_POST['tgl_klr']);
 
     // Prepared statement untuk menambah data
-    $query = $db->prepare("INSERT INTO `barang_klr`(`no_klr`, `nip`, `kd_brg`, `jml_klr`, `wkt_klr`, `tgl_klr`) VALUES (:no_klr, :nip, :kd_brg, :jml_klr, :wkt_klr, :tgl_klr)");
-    $query->bindParam(":no_klr", $kode);
+    $query = $db->prepare("UPDATE `barang_klr` SET `nip` = :nip,`kd_brg` = :kd_brg, `jml_klr` = :jml_klr, `wkt_klr` = :wkt_klr, `tgl_klr` = :tgl_klr WHERE no_klr = :no_klr");
     $query->bindParam(":nip", $currentUser['nip']);
     $query->bindParam(":kd_brg", $kd_brg);
     $query->bindParam(":jml_klr", $jml_klr);
     $query->bindParam(":wkt_klr", $wkt_klr);
     $query->bindParam(":tgl_klr", $tgl_klr);
+    $query->bindParam(":no_klr", $_GET['id']);
     // Jalankan perintah SQL
     $query->execute();
     // Alihkan ke index.php
@@ -104,20 +107,20 @@ if(isset($_POST['submit'])){
                 <div class="col-xs-12">
                     <div class="box">
                         <div class="box-header with-border">
-                            <h3 class="box-title">Tambah Barang Keluar</h3>
+                            <h3 class="box-title">Ubah Barang Keluar</h3>
                         </div>
                         <!-- /.box-header -->
                         <form method=post>
                             <div class="box-body">
                                 <div class="form-group">
                                     <label for="no_klr">Nomor Barang Keluar</label>
-                                    <input type="text" name="no_klr" id="no_klr" class="form-control" value="<?php echo $kode ?>" readonly>
+                                    <input type="text" name="no_klr" id="no_klr" class="form-control" value="<?php echo $data['no_klr'] ?>" readonly>
                                 </div>
                                 <div class="form-group">
                                     <label for="no_sp">Kode Barang dan Nama Barang</label>
                                     <!-- Perulangan Untuk Menampilkan Semua Data yang ada di Variable Data -->
                                     <select class="form-control select2" style="width: 100%;" name="kd_brg" id="kd_brg" required="">
-                                        <option value=""> </option>
+                                        <option value="<?php echo $data['kd_brg'] ?>"><?php echo $data['kd_brg'] ?> - <?php echo $data['nm_brg']; ?></option>
                                         <?php foreach ($data2 as $value): ?>
                                         <option value="<?php echo $value['kd_brg'] ?>"><?php echo $value['kd_brg'] ?> - <?php echo $value['nm_brg']; ?></option>
                                         <?php endforeach; ?>
@@ -125,13 +128,13 @@ if(isset($_POST['submit'])){
                                 </div>
                                 <div class="form-group">
                                     <label for="jml_klr">Jumlah Keluar</label>
-                                    <input type="number" name="jml_klr" id="jml_klr" class="form-control" value="" required="">
+                                    <input type="number" name="jml_klr" id="jml_klr" class="form-control" value="<?php echo $data['jml_klr'] ?>" required="">
                                 </div>
                                 <div class="bootstrap-timepicker">
                                     <div class="form-group">
                                         <label>Waktu Keluar</label>
                                         <div class="input-group">
-                                            <input type="text" class="form-control timepicker" name="wkt_klr" value="<?php echo $time ?>" required="">
+                                            <input type="text" class="form-control timepicker" name="wkt_klr" value="<?php echo $data['wkt_klr'] ?>" required="">
                                             <div class="input-group-addon">
                                                 <i class="fa fa-clock-o"></i>
                                             </div>
@@ -146,10 +149,10 @@ if(isset($_POST['submit'])){
                                     <div class="input-group-addon">
                                       <i class="fa fa-calendar"></i>
                                     </div>
-                                    <input type="text" class="form-control pull-right" id="datepicker" name="tgl_klr" value="<?php echo $date ?>" required="">
+                                    <input type="text" class="form-control pull-right" id="datepicker" name="tgl_klr" value="<?php echo $data['tgl_klr'] ?>" required="">
                                   </div>
                                 <div class="box-footer">
-                                    <button type="submit" class="btn btn-primary btn-flat" name="submit">Tambah</button>
+                                    <button type="submit" class="btn btn-primary btn-flat" name="submit">Ubah</button>
                                 </div>
                         </form>
                         <!-- /.box-body -->
